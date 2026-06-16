12c5d21a6b36a7017e527b1647f973014cc0bf68
pahcosta <pauloh.alvesdacosta@gmail.com>
2026-06-16 12:13:27 -0300

    chore: add jsdom polyfills to test setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,14 @@
 import '@testing-library/jest-dom'
 import { server } from './mocks/server'
 
+globalThis.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+window.HTMLElement.prototype.scrollIntoView = () => {}
+
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
